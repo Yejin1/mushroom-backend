4b03a60115ce4ce8956788c1ade85bba62447c7d
Yejin1 <dpwls273273@gmail.com>
2025-04-23 19:19:44 +0900

    버섯상사 전자결재 백엔드 생성

---
+++ src/test/java/dev/yejin/mushroom_backend/MushroomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yejin.mushroom_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MushroomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
