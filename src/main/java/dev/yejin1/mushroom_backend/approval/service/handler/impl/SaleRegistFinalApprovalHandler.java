eb0124917b18110019135dad70574b480666d1c9
Yejin1 <dpwls273273@gmail.com>
2025-11-03 16:53:00 +0900

    (수정) 매출양식 결재완료 시 메시지 발송 로직 추가(로컬용)

--- src/main/java/dev/yejin1/mushroom_backend/approval/service/handler/impl/SaleRegistFinalApprovalHandler.java
+++ src/main/java/dev/yejin1/mushroom_backend/approval/service/handler/impl/SaleRegistFinalApprovalHandler.java
@@ -1,15 +1,35 @@
 package dev.yejin1.mushroom_backend.approval.service.handler.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.yejin1.mushroom_backend.approval.repository.ApprovalDocBodyRepository;
 import dev.yejin1.mushroom_backend.approval.service.handler.FinalApprovalHandler;
 import dev.yejin1.mushroom_backend.approval.service.handler.model.ApprovalContext;
+import dev.yejin1.mushroom_backend.sale.service.SaleService;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Component;
 
 @Component("SALE_REGIST")
+@RequiredArgsConstructor
 public class SaleRegistFinalApprovalHandler implements FinalApprovalHandler {
+
+    private final SaleService saleService;
+    private final ApprovalDocBodyRepository approvalDocBodyRepository;
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     @Override
     public void handle(ApprovalContext ctx) {
-        // todo : 결재 완료 시 매출 등록 처리
+        // 판매 실적 등록 양식이면 redis 데이터 등록 (로컬 테스트용)
+//        String formContents = approvalDocBodyRepository.getById(ctx.getDoc().getId()).getFormContent();
+//        int amount;
+//        try {
+//            amount = MAPPER.readTree(formContents).path("amount").asInt();
+//        } catch (JsonProcessingException e) {
+//            throw new IllegalArgumentException("formContents JSON 파싱 실패", e);
+//        }
+//        saleService.handleSalesSubmitted(ctx.getDoc().getWriter(), amount);
+
         ctx.getDoc().setStatusCd(2);
         ctx.getDoc().setStatusNm("결재완료");
         ctx.getDoc().setCompletedDt(ctx.getNow());
