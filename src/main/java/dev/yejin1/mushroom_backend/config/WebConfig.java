acb593992b5818c68d2c378cdbc2501f82213224
Yejin1 <dpwls273273@gmail.com>
2025-11-07 20:08:45 +0900

    (수정) CORS 설정 변경 (patch 메소드 추가)

--- src/main/java/dev/yejin1/mushroom_backend/config/WebConfig.java
+++ src/main/java/dev/yejin1/mushroom_backend/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173","https://delightful-ocean-09cea8300.6.azurestaticapps.net","https://mushroom-doc.com")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
